5740dd20096aeb635036d669149364fc65f8828b
Bruna Pereira <bpereira@thoughtworks.com>
2018-11-22 17:26:39 -0200

    Create method to fetch last aired episode

--- shows/client/thetvdb.py
+++ shows/client/thetvdb.py
@@ -38,6 +38,20 @@ class TheTvDb(object):
         else:
             print ('some error occurred')
 
+    def find_last_episode(self, tv_show_id):
+        headers = {'Authorization': self.token}
+        response = requests.get(f'{self.base_url}/series/{tv_show_id}/episodes', headers=headers)
+
+        last_page = response.json()['links']['last']
+        response = requests.get(f'{self.base_url}/series/{tv_show_id}/episodes/query?page={last_page}', headers=headers)
+
+        last_aired = response.json()['data'][-1]
+        season = last_aired['airedSeason'] 
+        episode = str(last_aired['airedEpisodeNumber']).zfill(2)
+
+        return f'S{season}E{episode}'
+
+
 
 
 if __name__ == '__main__':
@@ -45,3 +59,4 @@ if __name__ == '__main__':
     client = TheTvDb()
     client.login()
     client.fetch_favorites_from_user()
+    client.find_last_episode('73762')
