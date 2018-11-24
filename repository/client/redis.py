63368f462e014f5f2227d7b2db8cd02b5fb07479
ederson badeca <edersonbadeca@gmail.com>
2018-11-24 16:23:42 -0200

    Uses env var for redis configurations

--- repository/client/redis.py
+++ repository/client/redis.py
@@ -1,5 +1,11 @@
 from redis import Redis
+from prettyconf import config
+
+host = config('REDIS_HOST')
+port = config('REDIS_PORT')
+db = config('REDIS_DB', cast=int)
+
 
 def save(key, value, number):
-    r = Redis(host='localhost', port=6379, db=0)
+    r = Redis(host=host, port=port, db=db)
     r.hset(key, value, number)
