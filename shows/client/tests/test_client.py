9187bc06507a79fd84573d759da2b7ee6a9efd0f
ederson badeca <edersonbadeca@gmail.com>
2018-11-24 17:17:31 -0200

    Creates tests for get_tv_show_name

--- shows/client/tests/test_client.py
+++ shows/client/tests/test_client.py
@@ -27,3 +27,10 @@ def test_fetch_favorites_from_user(client):
     client.login()
     response = client.fetch_favorites_from_user()
     assert len(response) > 0
+
+
+@vcr.use_cassette('shows/client/tests/fixtures/thetvdb/vcr/get_tv_show_name.yml')
+def test_get_tv_show_name(client):
+    client.login()
+    response = client.get_tv_show_name(73762)
+    assert len(response) > 0
