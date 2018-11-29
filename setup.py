85c58fbaa801c95cfcae528e8fe026e720f3e5c8
Bruna Pereira <bpereira@thoughtworks.com>
2018-11-29 14:52:21 -0200

    Create setup to deploy app

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='tvshowsdownloader',
+  packages=['tvshowsdownloader'],
+  version='1.0',
+  description='Using external APIs, download the following episodes of your favorite TV shows and the respective subtitles. Automatically.',
+  author='Bruna Pereira',
+  author_email='ppbruna2@gmail.com',
+  url='https://github.com/brunapereira/tv-shows-downloader', 
+  download_url= 'https://github.com/brunapereira/tv-shows-downloader',
+  keywords = ['tv shows', 'download'],
+  classifiers = [],
+)
